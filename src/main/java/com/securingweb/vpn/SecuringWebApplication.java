8616302e27bf5f5cefacdc9f081babb91f29f65a
jackLiu <54433489@qq.com>
2021-09-04 21:27:45 +0800

    display all bean when application starts

--- src/main/java/com/securingweb/vpn/SecuringWebApplication.java
+++ src/main/java/com/securingweb/vpn/SecuringWebApplication.java
@@ -3,6 +3,7 @@ package com.securingweb.vpn;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
 import java.io.IOException;
@@ -15,6 +16,7 @@ public class SecuringWebApplication {
 //        SpringApplication.run(SecuringWebApplication.class, args);
 
         ConfigurableApplicationContext ctx = new SpringApplication(SecuringWebApplication.class).run(args);
+        displayAllBeans(ctx);
         System.out.println("Hit Enter to terminate");
         try {
             System.in.read();
@@ -22,5 +24,12 @@ public class SecuringWebApplication {
             e.printStackTrace();
         }
         ctx.close();
+    }
+
+    public static void displayAllBeans(ApplicationContext applicationContext) {
+        String[] allBeanNames = applicationContext.getBeanDefinitionNames();
+        for (String beanName : allBeanNames) {
+            System.out.println(beanName);
+        }
     }
 }
